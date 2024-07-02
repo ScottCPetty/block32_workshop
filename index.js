42e58a8a23af1f3d0cb70e2051b3b9c5be3c8a32
ScottCPetty <scottishcheese13@gmail.com>
2024-07-02 12:47:55 -0400

    Created and seeded table

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ const app = express();
 async function init() {
   await client.connect();
   console.log("connected to database");
-  let SQL = ``;
+  let SQL = `
+    DROP TABLE IF EXISTS flavors;
+    CREATE TABLE flavors(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        is_favorite BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now(),
+    );
+  `;
   await client.query(SQL);
   console.log("tables created");
-  SQL = ``;
+  SQL = `
+    INSERT INTO notes(name, is_favorite) VALUES ('Chocolate', true);
+    INSERT INTO notes(name) VALUES('Vanilla');
+  `;
   await client.query(SQL);
   console.log("data seeded");
 }
